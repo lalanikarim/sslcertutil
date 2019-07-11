6089e2f8a0d732f6d955e5f3eaa8cabfb35a6083
Karim Lalani <jimmy00784@gmail.com>
2019-07-11 19:18:53 +0000

    added optional 2nd parameter for output pem file

--- installssl.sh
+++ installssl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ "$1" == "" ]; then
-	echo "Usage: $0 <server-domain>"
+if [ "$1" == "" ] || [ "$1" == "--help" ]; then
+	echo "Usage: $0 <server-domain> [<cert-outfile.pem>]"
 else
 	if [ -d "/etc/letsencrypt/live/$1" ]; then
-		cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > $1.pem
-		echo "SSL certificate for $1 updated"
+		PEMFILE="$1.pem"
+		if [ "$2" != "" ]; then
+			PEMFILE="$2"
+		fi
+		cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > $PEMFILE
+		echo "SSL certificate for $1 updated and stored to $PEMFILE"
 	else
 		echo "Unable to access /etc/letsencrypt/live/$1"
 	fi
